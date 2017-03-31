ea28f38312db448f40b615e8393d19dfcca66168
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2017-03-31 10:47:22 +0530

    UI connect

--- FindMe/ViewController.swift
+++ FindMe/ViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var lonLabel: UILabel!
+    @IBOutlet weak var altLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var courseLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
